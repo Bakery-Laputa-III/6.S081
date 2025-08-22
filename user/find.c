37ed644aeca10788b807f28d1e57a35abad78fc9
Bakery-Laputa-III <Bakery-Laputa-III@outlook.com>
2025-08-22 20:07:34 +0800

    find fin

---
+++ user/find.c
@@ -0,0 +1,77 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void
+find(const char *path, const char *filename) {
+    int fd;
+    struct dirent de;
+    struct stat st;
+    char buf[512], *p;
+
+    if ((fd = open(path, 0)) < 0) {
+        fprintf(2, "find: cannot open %s\n", path);
+        exit(1);
+    }
+
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "find cannot stat %s\n", path);
+        close(fd);
+        exit(1);
+    }
+
+    if (st.type != T_DIR) {
+        fprintf(2, "usage: find <DIRECTORY> <FILENAME>\n");
+        exit(1);
+    }
+
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+        fprintf(2, "find: path too long\n");
+        close(fd);
+        exit(1);
+    }
+
+    strcpy(buf, path);
+    p = buf + strlen(path);
+    *p++ = '/';
+
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+        if (de.inum == 0) {
+            continue;
+        }
+        
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        
+        if (stat(buf, &st) < 0) {
+            fprintf(2, "find: cannot stat %s\n", buf);
+            exit(1);
+        }
+
+        switch (st.type) {
+        case T_FILE:
+            if (strcmp(filename, p) == 0) {
+                fprintf(1, "%s\n", buf);
+            }
+            break;
+        case T_DIR:
+            if (strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
+                find(buf, filename);
+            }
+            break;
+        }
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(2, "Usage: find <PATH> <FILENAME>\n");
+        exit(1);
+    } 
+    
+    find(argv[1], argv[2]);
+
+    exit(0);
+}
